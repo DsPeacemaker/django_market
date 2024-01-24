6d48cfe58653939e5ec3cbf08f3c6980959e3912
DsPeacemaker <enotikpartizan@mail.ru>
2024-01-24 20:46:36 +0300

    added templates html base, index, about

--- main/views.py
+++ main/views.py
@@ -9,14 +9,16 @@ def index(request) -> HttpResponse:
     POST, GET, какой пользователей и тд
     """
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request) -> HttpResponse:
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст на странице о нас',
+    }
+    return render(request, 'main/about.html', context)
