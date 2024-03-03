1dd559a3f592d8e134e13b55246e1cf9a3eda27f
DsPeacemaker <enotikpartizan@mail.ru>
2024-03-03 20:50:47 +0300

    fixning errors with register new user

--- users/models.py
+++ users/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Аватар')
     phone_number = models.CharField(max_length=10, blank=True, null=True, verbose_name="Номер телефона")
-    favorites_id = models.PositiveIntegerField(default=0, verbose_name='Артикул товара')
-    favorites_name = models.CharField(max_length=150, unique=True, verbose_name='Название товара')
 
     class Meta:
         db_table = 'user'
